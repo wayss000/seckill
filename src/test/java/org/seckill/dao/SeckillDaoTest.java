10f1f3319de746b9d9e10b27bf2533a0550c2081
wayss000 <wayss000@gmail.com>
2017-03-28 00:03:53 +0800

    完成SeckillDao剩余的测试用例

--- src/test/java/org/seckill/dao/SeckillDaoTest.java
+++ src/test/java/org/seckill/dao/SeckillDaoTest.java
@@ -1,5 +1,8 @@
 package org.seckill.dao;
 
+import java.util.Date;
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.junit.Test;
@@ -26,20 +29,35 @@ public class SeckillDaoTest {
     
     @Test
     public void testReduceNumber() throws Exception{
-        long id=1000;
-        Seckill seckill = seckillDao.queryById(id);
-        System.out.println(seckill.getName());
-        System.out.println(seckill);
+        Date killTime = new Date();
+        int updateCount = seckillDao.reduceNumber(1000L,killTime);
+        System.out.println("updateCount=" + updateCount);
+        //结果：updateCount=0
     }
     
     @Test
     public void testQueryById() throws Exception{
-        
+        long id=1000;
+        Seckill seckill = seckillDao.queryById(id);
+        System.out.println(seckill.getName());
+        System.out.println(seckill);
+        //结果
+        //1000元秒杀iphone6s
+        //Seckill [seckillId=1000, name=1000元秒杀iphone6s, number=100, startTime=Tue Nov 01 00:00:00 CST 2016, endTime=Wed Nov 02 00:00:00 CST 2016, createTime=Sun Mar 26 22:40:20 CST 2017]
+
     }
     
     @Test
     public void testQueryAll() throws Exception{
-        
+        List<Seckill> seckills = seckillDao.queryAll(0,100);
+        for(Seckill seckill : seckills){
+            System.out.println(seckill);
+        }
+        //结果
+        //Seckill [seckillId=1000, name=1000元秒杀iphone6s, number=100, startTime=Tue Nov 01 00:00:00 CST 2016, endTime=Wed Nov 02 00:00:00 CST 2016, createTime=Sun Mar 26 22:40:20 CST 2017]
+        //Seckill [seckillId=1001, name=500元秒杀ipad2, number=100, startTime=Tue Nov 01 00:00:00 CST 2016, endTime=Wed Nov 02 00:00:00 CST 2016, createTime=Sun Mar 26 22:40:20 CST 2017]
+        //Seckill [seckillId=1002, name=300元秒杀小米4, number=100, startTime=Tue Nov 01 00:00:00 CST 2016, endTime=Wed Nov 02 00:00:00 CST 2016, createTime=Sun Mar 26 22:40:20 CST 2017]
+        //Seckill [seckillId=1003, name=200元秒杀红米note, number=100, startTime=Tue Nov 01 00:00:00 CST 2016, endTime=Wed Nov 02 00:00:00 CST 2016, createTime=Sun Mar 26 22:40:20 CST 2017]
     }
     
 }
