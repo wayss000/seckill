5592f5208e20f896c280eb461aba1cd9bceafb05
wayss000 <wayss000@gmail.com>
2017-03-28 00:03:19 +0800

    当mybatis中有多个参数时，需要用@Param注解

--- src/main/java/org/seckill/dao/SeckillDao.java
+++ src/main/java/org/seckill/dao/SeckillDao.java
@@ -3,6 +3,7 @@ package org.seckill.dao;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
 import org.seckill.entity.Seckill;
 
 /**
@@ -17,7 +18,7 @@ public interface SeckillDao {
      * @param killTime
      * @return 如果影响行数>1,表示更新的记录行数
      */
-    int reduceNumber(long seckillId, Date killTime);
+    int reduceNumber(@Param("seckillId")long seckillId, @Param("killTime")Date killTime);
     
     /**
      * 根据id查询秒杀对象
@@ -32,5 +33,6 @@ public interface SeckillDao {
      * @param limit
      * @return
      */
-    List<Seckill> queryAll(int offet, int limit);
+    //这个借口有两个参数，mybatis需要注解才能识别出
+    List<Seckill> queryAll(@Param("offset")int offet, @Param("limit")int limit);
 }
