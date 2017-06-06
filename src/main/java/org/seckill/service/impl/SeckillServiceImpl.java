8c3d1d0a0b919e391558b4faba1765f8dba84b29
wayss000 <wayss000@gmail.com>
2017-06-06 23:35:59 +0800

    提交接口实现

---
+++ src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
@@ -0,0 +1,12 @@
+package org.seckill.service.impl;
+
+import org.seckill.service.SeckillService;
+
+/**
+* @author Wayss
+* @version 2017年6月5日 上午12:08:25
+*/
+
+public class SeckillServiceImpl implements SeckillService{
+
+}
